c28b5dc473c0b27ab0b981c012444e347c6c0770
charlespv <charlespv@hotmail.fr>
2018-02-02 12:27:41 +0100

    première version du front.js

---
+++ routes/front.js
@@ -0,0 +1,21 @@
+/*
+Importer les composants de la route
+*/
+const express = require('express');
+const router = express.Router();
+//
+
+/*
+Définition des routes
+*/
+router.get( '/', (req, res) => {
+    // Renvoyer le fichier index dans la réponse
+    res.render('index');
+});
+//
+
+/*
+Exporter le module de route
+*/
+module.exports = router;
+//
